ed2cfd1ad788bd2d9260d3c71051a7cf16ec5857
Biniyam Taye <biniyamtaye04@gmail.com>
2026-07-13 17:31:56 +0300

    feat: implement home screen UI with featured rooms and services sections

--- hotel_app/lib/features/home/presentation/screens/home_screen.dart
+++ hotel_app/lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../../../core/theme/app_spacing.dart';
 import '../../../../core/widgets/inputs/search_input.dart';
 
