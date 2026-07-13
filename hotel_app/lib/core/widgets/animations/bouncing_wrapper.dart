736c3f1e2eb92f16d754e8b24441428c046a81bc
Biniyam Taye <biniyamtaye04@gmail.com>
2026-07-13 18:10:53 +0300

    feat: add BouncingWrapper widget for interactive tap-to-scale animations

---
+++ hotel_app/lib/core/widgets/animations/bouncing_wrapper.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class BouncingWrapper extends StatefulWidget {
+  final Widget child;
+  final VoidCallback? onTap;
+  final double scaleFactor;
+  final Duration duration;
+
+  const BouncingWrapper({
+    super.key,
+    required this.child,
+    this.onTap,
+    this.scaleFactor = 0.96,
+    this.duration = const Duration(milliseconds: 150),
+  });
+
+  @override
+  State<BouncingWrapper> createState() => _BouncingWrapperState();
+}
+
+class _BouncingWrapperState extends State<BouncingWrapper> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+    _scaleAnimation = Tween<double>(begin: 1.0, end: widget.scaleFactor).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    if (widget.onTap != null) _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    if (widget.onTap != null) {
+      _controller.reverse();
+      widget.onTap!();
+    }
+  }
+
+  void _onTapCancel() {
+    if (widget.onTap != null) _controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: widget.child,
+      ),
+    );
+  }
+}
